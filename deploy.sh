2cebe1886e0be0bdd2903ba3e0c1560042478ee5
Christian Manteuffel <cm@notagain.de>
2016-11-23 16:13:21 +0100

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+echo -e "\033[0;32mBulding site...\033[0m"
+hugo
 echo -e "\033[0;32mSetting user rights...\033[0m"
-
 chmod -R 755 ./public
 echo -e "\033[0;32mDeploying updates...\033[0m"
 lftp -c "open -u $FTP_USER,$FTP_PASSWORD ftp.manteuffel.info; set ssl:verify-certificate no; mirror -R ./public /webseiten/christian"
