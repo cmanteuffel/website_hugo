420d683ecf4e581dfc1b2a39d0d1663fe6644003
Christian Manteuffel <cm@notagain.de>
2016-11-23 16:09:13 +0100

    Travis install lftp

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-echo -e "\033[0;32mBulding site...\033[0m"
-
-hugo
-
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo -e "\033[0;32mSetting user rights...\033[0m"
 
 chmod -R 755 ./public
+echo -e "\033[0;32mDeploying updates...\033[0m"
 lftp -c "open -u $FTP_USER,$FTP_PASSWORD ftp.manteuffel.info; set ssl:verify-certificate no; mirror -R ./public /webseiten/christian"
